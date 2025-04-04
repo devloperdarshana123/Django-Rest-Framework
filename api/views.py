68770a02d1d5a8b97e8165004346f213f043be3f
developerdarshana123 <23115667@hitdoon.com>
2025-04-04 21:28:14 +0530

    Django Filter Installation

--- api/views.py
+++ api/views.py
@@ -11,6 +11,7 @@ from django.http import Http404
 from rest_framework import mixins , generics , viewsets
 from blogs.models import Blog , Comment
 from blogs.serializers import BlogSerializer , CommentSerializer
+from .paginations import CustomPagination
 
 @api_view(['GET' ,'POST'])
 def studentsView(request):
@@ -147,6 +148,7 @@ def studentsDetailView(request , pk):
 class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    pagination_class = CustomPagination
 
 
 class BlogsView(generics.ListCreateAPIView):
