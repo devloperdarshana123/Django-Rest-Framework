a43a4bd8a5c99525dc1fbcea6eff953b07f19183
developerdarshana123 <23115667@hitdoon.com>
2025-04-07 20:26:20 +0530

    Filters IN Django Rest Framework

--- api/views.py
+++ api/views.py
@@ -12,6 +12,7 @@ from rest_framework import mixins , generics , viewsets
 from blogs.models import Blog , Comment
 from blogs.serializers import BlogSerializer , CommentSerializer
 from .paginations import CustomPagination
+from employees.filters import  EmployeeFilter
 
 @api_view(['GET' ,'POST'])
 def studentsView(request):
@@ -149,6 +150,7 @@ class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     pagination_class = CustomPagination
+    filterset_class = EmployeeFilter
 
 
 class BlogsView(generics.ListCreateAPIView):
